17ede862d73151179474fde8120977308c89fd1a
Iliyan Peychev <iliyan.peychev@liferay.com>
2013-02-01 12:45:10 +0100

    isShuffle modified to cache the strings which cannot produce valid shuffle

--- strings/is-shuffle/is-shuffle.js
+++ strings/is-shuffle/is-shuffle.js
@@ -11,6 +11,16 @@ Liferay.IsShuffle.prototype = {
 	constructor: Liferay.IsShuffle,
 
 	check: function(str1, str2, str3) {
+		var hashtable = Object.create ? Object.create(null) : {};
+
+		return this._check(str1, str2, str3, hashtable);
+	},
+
+	_check: function(str1, str2, str3, hashtable) {
+		if (hashtable[str1 + str2]) {
+			return false;
+		}
+
 		if (str1.length + str2.length !== str3.length) {
 			return false;
 		}
@@ -24,11 +34,13 @@ Liferay.IsShuffle.prototype = {
 		}
 
 		if (str1.charAt(0) === str3.charAt(0)) {
-			return this.check(str1.substr(1), str2, str3.substr(1));
+			return this.check(str1.substr(1), str2, str3.substr(1), hashtable);
 		}
 		else {
-			return this.check(str1, str2.substr(1), str3.substr(1));
+			return this.check(str1, str2.substr(1), str3.substr(1), hashtable);
 		}
+
+		hashtable[str1 + str2] = true;
 
 		return false;
 	}
