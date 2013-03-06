4b1833cb0239fe68803736b97c4a5557e12d0c42
Iliyan Peychev <iliyan.peychev@liferay.com>
2013-03-06 18:25:14 +0100

    Test

---
+++ strings/lowest-common-substring/lowest-common-substring.js
@@ -0,0 +1,16 @@
+'use strict';
+
+if (typeof Liferay == 'undefined') {
+	var Liferay = {};
+}
+
+Liferay.LowestCommonSubstring = function(config) {
+};
+
+Liferay.LowestCommonSubstring.prototype = {
+	constructor: Liferay.LowestCommonSubstring,
+
+	find: function(arrayStrings) {
+		
+	}
+};
