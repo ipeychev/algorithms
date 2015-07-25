7f9180761ad0b94a656e10e246d3a48e9de263f3
Iliyan Peychev <iliyan.peychev@gmail.com>
2015-07-25 07:47:36 +0200

    Use the original module name

--- test/strings/reverse-sentence/test.js
+++ test/strings/reverse-sentence/test.js
@@ -1,17 +1,17 @@
 var assert = require('assert');
 var path = require('path');
-var ReverseWords = require(path.resolve(__dirname, '../../../src/strings/reverse-sentence/reverse-sentence.js'));
+var ReverseSentence = require(path.resolve(__dirname, '../../../src/strings/reverse-sentence/reverse-sentence.js'));
 
 describe('ReverseSentence', function() {
     it('should reverse a sentence', function() {
-        assert.strictEqual('string is this', ReverseWords('this is string'));
+        assert.strictEqual('string is this', ReverseSentence('this is string'));
     });
 
     it('should reverse a sentence with multiple spaces', function() {
-        assert.strictEqual('string  is   this', ReverseWords('this   is  string'));
+        assert.strictEqual('string  is   this', ReverseSentence('this   is  string'));
     });
 
     it('should reverse a sentence with one word', function() {
-        assert.strictEqual('string', ReverseWords('string'));
+        assert.strictEqual('string', ReverseSentence('string'));
     });
 });
